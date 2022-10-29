df5b081e6c3535cdd629e270cdf77741c840f34a
Sipi Ovaska <ovaskasi@gmail.com>
2022-10-29 08:41:08 +0300

    chore: add arg guard for enums

---
+++ FlameCsv.Core/Extensions/GuardEx.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Diagnostics;
+
+namespace FlameCsv.Extensions;
+
+internal static class GuardEx
+{
+    public static void IsDefined<TEnum>(TEnum value, [CallerArgumentExpression("value")] string name = "")
+        where TEnum : struct, Enum
+    {
+        if (!Enum.IsDefined(value))
+            ThrowHelper.ThrowArgumentOutOfRangeException(name, value, null);
+    }
+}
