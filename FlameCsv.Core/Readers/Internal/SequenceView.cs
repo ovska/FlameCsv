7b58f1ec9f27c902a520f37b81233a4c42c8d49e
Sipi Ovaska <ovaskasi@gmail.com>
2022-11-01 08:33:44 +0200

    refactor: trim sequenceview

--- FlameCsv.Core/Readers/Internal/SequenceView.cs
+++ FlameCsv.Core/Readers/Internal/SequenceView.cs
@@ -1,19 +1,34 @@
 using System.Buffers;
+using System.Diagnostics;
+using System.Text;
 using FlameCsv.Extensions;
 
 namespace FlameCsv.Readers.Internal;
 
-internal readonly ref struct SequenceView<T> where T : unmanaged
+/// <summary>
+/// A non-allocating view around a <see cref="ReadOnlySequence{T}"/> that copies the
+/// data to a pooled buffer if it is multisegment.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[DebuggerDisplay(@"\{ SequenceView: [{DebugString,nq}] \}")]
+internal readonly struct SequenceView<T> : IDisposable
+    where T : unmanaged, IEquatable<T>
 {
-    public ReadOnlySpan<T> Span { get; }
+    /// <summary>
+    /// Data in the parameter sequence trimmed according to options.
+    /// </summary>
+    public ReadOnlyMemory<T> Memory { get; }
+
     private readonly T[]? _array;
     private readonly bool _clearArray;
 
-    public SequenceView(ReadOnlySequence<T> sequence, SecurityLevel security)
+    public SequenceView(
+        in ReadOnlySequence<T> sequence,
+        CsvConfiguration<T> configuration)
     {
         if (sequence.IsSingleSegment)
         {
-            Span = sequence.FirstSpan;
+            Memory = sequence.First;
             _array = default;
             _clearArray = default;
         }
@@ -22,9 +37,11 @@ internal readonly ref struct SequenceView<T> where T : unmanaged
             int length = (int)sequence.Length;
             _array = ArrayPool<T>.Shared.Rent(length);
             sequence.CopyTo(_array);
-            Span = _array.AsSpan(0, length);
-            _clearArray = security.ClearBuffers();
+            Memory = _array.AsMemory(0, length);
+            _clearArray = configuration.Security.ClearBuffers();
         }
+
+        Memory = Memory.Trim(configuration.options.Whitespace.Span);
     }
 
     public void Dispose()
@@ -32,4 +49,12 @@ internal readonly ref struct SequenceView<T> where T : unmanaged
         if (_array is not null)
             ArrayPool<T>.Shared.Return(_array, _clearArray);
     }
+
+#if DEBUG
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebugString
+        => typeof(T) == typeof(byte)
+            ? Encoding.UTF8.GetString(((ReadOnlyMemory<byte>)(object)Memory).Span)
+            : Memory.Span.ToString();
+#endif
 }
