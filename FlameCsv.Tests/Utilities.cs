108fa45d90850f28f36591b61a0f2c40fc6b3421
Sipi Ovaska <ovaskasi@gmail.com>
2022-11-01 08:51:51 +0200

    test: use buffersize in synchronous CsvReader tests as well

--- FlameCsv.Tests/Utilities.cs
+++ FlameCsv.Tests/Utilities.cs
@@ -20,4 +20,31 @@ internal class MemorySegment<T> : ReadOnlySequenceSegment<T>
 
         return segment;
     }
+
+    /// <summary>
+    /// Buffers the parameter data into segments of specified max length.
+    /// </summary>
+    /// <param name="data">Source data</param>
+    /// <param name="bufferSize">Segment max length, -1 to always return a single segment</param>
+    public static ReadOnlySequence<T> Create(ReadOnlyMemory<T> data, int bufferSize)
+    {
+        if (bufferSize == -1 || data.Length <= bufferSize)
+        {
+            return new ReadOnlySequence<T>(data);
+        }
+
+        MemorySegment<T> first = new MemorySegment<T>(data.Slice(0, bufferSize));
+        MemorySegment<T> last = first;
+
+        ReadOnlyMemory<T> remaining = data.Slice(bufferSize);
+
+        while (remaining.Length > bufferSize)
+        {
+            last = last.Append(remaining.Slice(0, bufferSize));
+            remaining = remaining.Slice(bufferSize);
+        }
+
+        last = last.Append(remaining);
+        return new(first, 0, last, last.Memory.Length);
+    }
 }
