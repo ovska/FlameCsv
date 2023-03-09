b7462f22cfae83d75580f8d3c2c7ea4fb83e4d52
ovska <ovaskasi@gmail.com>
2023-03-09 12:15:49 +0200

    feat: add builtin tuple binding support

---
+++ FlameCsv.Tests/Binding/BuiltinBindingTests.cs
@@ -0,0 +1,32 @@
+﻿using FlameCsv.Readers;
+
+namespace FlameCsv.Tests.Binding;
+
+public static class BuiltinBindingTests
+{
+    private const string data = "1,true,Alice\r\n2,false,Bob\r\n";
+
+    [Fact]
+    public static void Should_Bind_To_ValueTuple()
+    {
+        var items = CsvReader
+            .Read<(int, bool, string)>(data, CsvTextReaderOptions.Default)
+            .ToList();
+
+        Assert.Equal(2, items.Count);
+        Assert.Equal((1, true, "Alice"), items[0]);
+        Assert.Equal((2, false, "Bob"), items[1]);
+    }
+
+    [Fact]
+    public static void Should_Bind_To_Tuple()
+    {
+        var items = CsvReader
+            .Read<Tuple<int, bool, string>>(data, CsvTextReaderOptions.Default)
+            .ToList();
+
+        Assert.Equal(2, items.Count);
+        Assert.Equal(new Tuple<int, bool, string>(1, true, "Alice"), items[0]);
+        Assert.Equal(new Tuple<int, bool, string>(2, false, "Bob"), items[1]);
+    }
+}
