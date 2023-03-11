f3e07bb6e8184f0d32f77eec6d377b0762d50d18
ovska <ovaskasi@gmail.com>
2023-03-11 22:51:26 +0200

    chore: add tests for csvprocessors

---
+++ FlameCsv.Tests/Readers/CsvProcessorTests.cs
@@ -0,0 +1,13 @@
+﻿using FlameCsv.Reading;
+
+namespace FlameCsv.Tests.Readers;
+
+public static class CsvProcessorTests
+{
+    [Fact]
+    public static void Default_CsvProcessor_Dispose_Should_Not_Throw()
+    {
+        Assert.Null(Record.Exception(() => default(CsvProcessor<char, object>).Dispose()));
+        Assert.Null(Record.Exception(() => default(CsvHeaderProcessor<char, object>).Dispose()));
+    }
+}
