af8340d22c93640dd7715c7f79c8243ed5690d1d
ovska <ovaskasi@gmail.com>
2024-06-29 20:51:44 +0300

    chore: delete unused polyfill

--- FlameCsv.Core/Properties/AssemblyInfo.cs
+++ FlameCsv.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("FlameCsv.Benchmark")]
 [assembly: InternalsVisibleTo("FlameCsv.Tests")]
 
-#if DEBUG
 [assembly: DebuggerDisplay(
     @"\{ StreamPipeReader, Buffered: {_bufferedBytes}, Stream completed: {_isStreamCompleted} \}",
     TargetTypeName = "System.IO.Pipelines.StreamPipeReader, System.IO.Pipelines, Version=6.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51")]
 [assembly: DebuggerDisplay(
     @"\{ ReadResult, Buffer: {Buffer.Length}, IsCompleted: {IsCompleted} \}",
     Target = typeof(System.IO.Pipelines.ReadResult))]
-#endif
