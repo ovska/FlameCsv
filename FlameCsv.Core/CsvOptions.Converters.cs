e8dd484de2c109a69d7590f3af705c9310a6baea
ovska <ovaskasi@gmail.com>
2025-02-06 15:26:49 +0200

    feat: AOT safe wrapper around converters

--- FlameCsv.Core/CsvOptions.Converters.cs
+++ FlameCsv.Core/CsvOptions.Converters.cs
@@ -170,40 +170,6 @@ partial class CsvOptions<T>
         return false;
     }
 
-    private bool TryGetExistingOrCustomConverter<TValue>(
-        [NotNullWhen(true)] out CsvConverter<T, TValue>? converter)
-    {
-        MakeReadOnly();
-
-        if (_converterCache.TryGetValue(typeof(TValue), out var cached) &&
-            cached is CsvConverter<T, TValue> casted)
-        {
-            converter = casted;
-            return true;
-        }
-
-        var local = _converters;
-
-        // null of the Converters-property is never accessed (no custom converters)
-        if (local is not null)
-        {
-            ReadOnlySpan<CsvConverter<T>> converters = local.Span;
-
-            // Read converters in reverse order so parser added last has the highest priority
-            for (int i = converters.Length - 1; i >= 0; i--)
-            {
-                if (converters[i] is CsvConverter<T, TValue> converterOfT)
-                {
-                    _converterCache.TryAdd(typeof(TValue), converter = converterOfT);
-                    return true;
-                }
-            }
-        }
-
-        converter = null;
-        return false;
-    }
-
     [RDC(Messages.ConverterFactories), RUF(Messages.ConverterFactories)]
     private bool TryCreateDefaultConverter(Type type, [NotNullWhen(true)] out CsvConverter<T>? converter)
     {
@@ -241,129 +207,10 @@ partial class CsvOptions<T>
         return false;
     }
 
-    #region Source Generator
-
-    /// <summary>
-    /// Returns a converter for <typeparamref name="TValue"/>, either a configured one or from the factory.
-    /// </summary>
-    /// <remarks>
-    /// This API is meant to be used by the source generator to produce trimming/AOT safe code.<br/>
-    /// If a non-factory user defined converter is found, it is returned directly.
-    /// </remarks>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public CsvConverter<T, TValue> GetOrCreate<TValue>(
-        [RequireStaticDelegate] Func<CsvOptions<T>, CsvConverter<T, TValue>> factory,
-        bool canCache = false)
-    {
-        ArgumentNullException.ThrowIfNull(factory);
-
-        if (TryGetExistingOrCustomConverter<TValue>(out CsvConverter<T, TValue>? converter))
-        {
-            return converter;
-        }
-
-        converter = factory(this);
-
-        if (converter is null) InvalidConverter.Throw(factory, typeof(TValue));
-
-        if (canCache)
-        {
-            CheckConverterCacheSize();
-            _converterCache.TryAdd(typeof(TValue), converter);
-        }
-
-        return converter;
-    }
-
-    /// <summary>
-    /// Returns a nullable converter, falling back to the built-in one if <see cref="UseDefaultConverters"/> is true.
-    /// </summary>
-    /// <remarks>
-    /// This API is meant to be used by the source generator to produce trimming/AOT safe code.<br/>
-    /// If a non-factory user defined converter is found, it is returned directly.
-    /// </remarks>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public CsvConverter<T, TValue?> GetOrCreateNullable<TValue>(
-        [RequireStaticDelegate] Func<CsvOptions<T>, CsvConverter<T, TValue>> factory,
-        bool canCache = false)
-        where TValue : struct
-    {
-        if (TryGetExistingOrCustomConverter(out CsvConverter<T, TValue?>? converter))
-        {
-            return converter;
-        }
-
-        if (!UseDefaultConverters) CsvConverterMissingException.Throw(typeof(TValue?));
-
-        CsvConverter<T, TValue> inner = GetOrCreate(factory);
-        if (inner is null) InvalidConverter.Throw(factory, typeof(TValue));
-
-        var result = TrimmableNullableConverter.Create(inner, GetNullToken(typeof(TValue)));
-
-        if (canCache)
-        {
-            CheckConverterCacheSize();
-            _converterCache.TryAdd(typeof(TValue?), result);
-        }
-
-        return result;
-    }
-
-    /// <summary>
-    /// Returns an enum converter, falling back to the built-in one if <see cref="UseDefaultConverters"/> is true.
-    /// </summary>
-    /// <remarks>
-    /// This API is meant to be used by the source generator to produce trimming/AOT safe code.<br/>
-    /// If a non-factory user defined converter is found, it is returned directly.
-    /// </remarks>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public CsvConverter<T, TEnum> GetOrCreateEnum<TEnum>() where TEnum : struct, Enum
-    {
-        if (typeof(T) == typeof(char))
-        {
-            return (CsvConverter<T, TEnum>)(object)Unsafe
-                .As<CsvOptions<char>>(this)
-                .GetOrCreate(
-                    static o =>
-                    {
-                        if (!o.UseDefaultConverters) CsvConverterMissingException.Throw(typeof(TEnum));
-                        return new EnumTextConverter<TEnum>(o);
-                    },
-                    canCache: true);
-        }
-
-        if (typeof(T) == typeof(byte))
-        {
-            return (CsvConverter<T, TEnum>)(object)Unsafe
-                .As<CsvOptions<byte>>(this)
-                .GetOrCreate(
-                    static o =>
-                    {
-                        if (!o.UseDefaultConverters) CsvConverterMissingException.Throw(typeof(TEnum));
-                        return new EnumUtf8Converter<TEnum>(o);
-                    },
-                    canCache: true);
-        }
-
-        throw new NotSupportedException("Enum converters are only supported for char and byte");
-    }
-
-#endregion
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private void CheckConverterCacheSize()
     {
         if (_converterCache.Count >= MaxConverterCacheSize) _converterCache.Clear();
-    }
-}
-
-file static class InvalidConverter
-{
-    [DoesNotReturn, MethodImpl(MethodImplOptions.NoInlining)]
-    public static void Throw(object factory, Type toConvert)
-    {
-        throw new CsvConfigurationException(
-            $"{factory.GetType().FullName} returned an invalid converter for {toConvert.FullName}");
     }
 }
 
---
+++ FlameCsv.SourceGen/Models/NestedType.cs
@@ -0,0 +1,50 @@
+﻿using FlameCsv.SourceGen.Helpers;
+
+namespace FlameCsv.SourceGen.Models;
+
+internal readonly record struct NestedType
+{
+    public required bool IsReadOnly { get; init; }
+    public required bool IsRefLikeType { get; init; }
+    public required bool IsAbstract { get; init; }
+    public required bool IsValueType { get; init; }
+    public required string Name { get; init; }
+
+    public static EquatableArray<NestedType> Parse(ITypeSymbol containingClass)
+    {
+        INamedTypeSymbol? type = containingClass.ContainingType;
+
+        if (type is null) return [];
+
+        List<NestedType> wrappers = PooledList<NestedType>.Acquire();
+
+        while (type is not null)
+        {
+            wrappers.Add(
+                new NestedType
+                {
+                    IsReadOnly = type.IsReadOnly,
+                    IsRefLikeType = type.IsRefLikeType,
+                    IsAbstract = type.IsAbstract,
+                    IsValueType = type.IsValueType,
+                    Name = type.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)
+                });
+
+            type = type.ContainingType;
+        }
+
+        wrappers.Reverse();
+        return wrappers.ToEquatableArrayAndFree();
+    }
+
+    public void WriteTo(IndentedTextWriter writer)
+    {
+        writer.WriteIf(IsReadOnly, "readonly ");
+        writer.WriteIf(IsRefLikeType, "ref ");
+        writer.WriteIf(IsAbstract, "abstract ");
+        writer.Write("partial ");
+        writer.Write(IsValueType ? "struct " : "class ");
+        writer.Write(Name);
+        writer.WriteLine(" {");
+    }
+}
