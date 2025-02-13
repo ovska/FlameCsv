4241e3ae0436c7cb8a85244e7d8ddd9d024488c1
ovska <ovaskasi@gmail.com>
2025-02-13 12:58:49 +0200

    refactor: add minimum buffer size when writing

--- FlameCsv.Core/Writing/CsvCharBufferWriter.cs
+++ FlameCsv.Core/Writing/CsvCharBufferWriter.cs
@@ -48,9 +48,9 @@ internal sealed class CsvCharBufferWriter : ICsvBufferWriter<char>
 
         _writer = writer;
         _allocator = allocator;
-        _bufferSize = bufferSize;
         _leaveOpen = leaveOpen;
-        _memoryOwner = allocator.Rent(bufferSize);
+        _bufferSize = Math.Max(256, bufferSize);
+        _memoryOwner = allocator.Rent(_bufferSize);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
