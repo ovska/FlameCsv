b6301f9a1573c895cbef1e377e81c657485c5724
ovska <ovaskasi@gmail.com>
2025-02-15 07:04:33 +0200

    perf: use buffer directly from constant data MemoryStream and StringReader

--- FlameCsv.Core/CsvReader.cs
+++ FlameCsv.Core/CsvReader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Buffers;
 using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
 using FlameCsv.Extensions;
 using FlameCsv.Reading;
 using JetBrains.Annotations;
@@ -21,12 +22,58 @@ public static partial class CsvReader
     /// <summary>
     /// Creates a PipeReader from a Stream.
     /// </summary>
-    private static PipeReaderWrapper CreatePipeReader(
+    internal static ICsvPipeReader<byte> CreatePipeReader(
         Stream stream,
         MemoryPool<byte> memoryPool,
         bool leaveOpen)
     {
+        if (stream is MemoryStream memoryStream && memoryStream.TryGetBuffer(out ArraySegment<byte> buffer))
+        {
+            return new ConstantPipeReader<byte>(
+                buffer,
+                stream,
+                leaveOpen,
+                static (stream, advanced) => ((MemoryStream)stream).Seek(advanced, SeekOrigin.Current));
+        }
+
         Guard.CanRead(stream);
-        return new(PipeReader.Create(stream, new StreamPipeReaderOptions(pool: memoryPool, leaveOpen: leaveOpen)));
+        return new PipeReaderWrapper(
+            PipeReader.Create(stream, new StreamPipeReaderOptions(pool: memoryPool, leaveOpen: leaveOpen)));
+    }
+
+    /// <summary>
+    /// Creates a PipeReader from a TextReader.
+    /// </summary>
+    /// <returns></returns>
+    internal static ICsvPipeReader<char> CreatePipeReader(
+        TextReader reader,
+        MemoryPool<char> memoryPool,
+        int bufferSize)
+    {
+        // zero-copy reads if the reader is a StringReader and hasn't been read yet
+        if (reader.GetType() == typeof(StringReader))
+        {
+            var stringReader = (StringReader)reader;
+            string? content = GetString(stringReader);
+
+            // not yet disposed
+            if (content is not null)
+            {
+                ReadOnlyMemory<char> data = content.AsMemory(start: GetPosition(stringReader));
+                return new ConstantPipeReader<char>(
+                    data,
+                    reader,
+                    leaveOpen: false,
+                    static (reader, advanced) => GetPosition((StringReader)reader) += (int)advanced);
+            }
+        }
+
+        return new TextPipeReader(reader, bufferSize, memoryPool);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_s")]
+        static extern ref string? GetString(StringReader stringReader);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_pos")]
+        static extern ref int GetPosition(StringReader stringReader);
     }
 }
--- FlameCsv.Core/Reading/ICsvPipeReader.cs
+++ FlameCsv.Core/Reading/ICsvPipeReader.cs
@@ -1,7 +1,4 @@
-﻿using System.Buffers;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
 
 namespace FlameCsv.Reading;
 
@@ -22,35 +19,4 @@ public interface ICsvPipeReader<T> : IDisposable, IAsyncDisposable where T : unm
     /// <param name="consumed">End position of the consumed data from the last read.</param>
     /// <param name="examined">End position of the examined data from the last read.</param>
     void AdvanceTo(SequencePosition consumed, SequencePosition examined);
-}
-
-/// <summary>
-/// Internal implementation detail.
-/// </summary>
-/// <param name="buffer">Read data</param>
-/// <param name="isCompleted">Whether any more data can be read from the reader after this</param>
-[DebuggerDisplay(@"\{ ReadResult<{typeof(T).Name,nq}> Length: {Buffer.Length}, IsCompleted: {IsCompleted} \}")]
-[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-[EditorBrowsable(EditorBrowsableState.Advanced)]
-public readonly struct CsvReadResult<T>(in ReadOnlySequence<T> buffer, bool isCompleted) where T : unmanaged, IBinaryInteger<T>
-{
-    /// <summary>
-    /// Data read from the data source, e.g., PipeReader or TextReader.
-    /// </summary>
-    public ReadOnlySequence<T> Buffer { get; } = buffer;
-
-    /// <summary>
-    /// If true, no more data can be read from the data source and all further reads will return an empty buffer.
-    /// </summary>
-    public bool IsCompleted { get; } = isCompleted;
-
-    /// <summary>
-    /// Deconstructs the result into its components.
-    /// </summary>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Deconstruct(out ReadOnlySequence<T> buffer, out bool isCompleted)
-    {
-        buffer = Buffer;
-        isCompleted = IsCompleted;
-    }
 }
