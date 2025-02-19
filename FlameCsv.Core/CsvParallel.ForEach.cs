e32047a927f982981cfbe9d95ec00251283e19b6
ovska <ovaskasi@gmail.com>
2025-02-19 22:40:32 +0200

    chore: put parallel behind a defconstant

---
+++ FlameCsv.Core/CsvParallel.ForEach.cs
@@ -0,0 +1,87 @@
+﻿#if FEATURE_PARALLEL
+using System.Buffers;
+using FlameCsv.Reading;
+using FlameCsv.Reading.Parallel;
+
+namespace FlameCsv;
+
+public static partial class CsvParallel
+{
+    private readonly struct ForEachInvoker<T>(Action<CsvFieldsRef<T>, CsvParallelState> action)
+        : ICsvParallelTryInvoke<T, object?>
+        where T : unmanaged, IBinaryInteger<T>
+    {
+        public bool TryInvoke(
+            scoped ref CsvFieldsRef<T> fields,
+            in CsvParallelState state,
+            out object? result)
+        {
+            action(fields, state);
+            result = null;
+            return true;
+        }
+    }
+
+    public static void ForEach(
+        string? csv,
+        Action<CsvFieldsRef<char>, CsvParallelState> action,
+        CsvOptions<char>? options = null,
+        ParallelOptions? parallelOptions = null)
+    {
+        ForEachCore(CsvPipeReader.Create(csv), action, options, parallelOptions);
+    }
+
+    public static void ForEach(
+        ReadOnlyMemory<char> csv,
+        Action<CsvFieldsRef<char>, CsvParallelState> action,
+        CsvOptions<char>? options = null,
+        ParallelOptions? parallelOptions = null)
+    {
+        ForEachCore(CsvPipeReader.Create(csv), action, options, parallelOptions);
+    }
+
+    public static void ForEach(
+        in ReadOnlySequence<char> csv,
+        Action<CsvFieldsRef<char>, CsvParallelState> action,
+        CsvOptions<char>? options = null,
+        ParallelOptions? parallelOptions = null)
+    {
+        ForEachCore(CsvPipeReader.Create(in csv), action, options, parallelOptions);
+    }
+
+    public static void ForEach(
+        TextReader textReader,
+        Action<CsvFieldsRef<char>, CsvParallelState> action,
+        CsvOptions<char>? options = null,
+        CsvReaderOptions readerOptions = default,
+        ParallelOptions? parallelOptions = null)
+    {
+        options ??= CsvOptions<char>.Default;
+        ForEachCore(
+            CsvPipeReader.Create(textReader, options._memoryPool, readerOptions),
+            action,
+            options,
+            parallelOptions);
+    }
+
+    private static void ForEachCore<T>(
+        ICsvPipeReader<T> reader,
+        Action<CsvFieldsRef<T>, CsvParallelState> action,
+        CsvOptions<T>? options = null,
+        ParallelOptions? parallelOptions = null)
+        where T : unmanaged, IBinaryInteger<T>
+    {
+        ArgumentNullException.ThrowIfNull(reader);
+        ArgumentNullException.ThrowIfNull(action);
+
+        Parallel.ForEach(
+            Core(CsvParser.Create(options ?? CsvOptions<T>.Default, reader)),
+            (iter, state) =>
+            {
+                CsvFields<T> fields = iter.Fields;
+                CsvFieldsRef<T> fieldsRef = new(in fields, iter.GetBuffer);
+                action(fieldsRef, new CsvParallelState { Index = iter.Index, Header = iter.Header, LoopState = state });
+            });
+    }
+}
+#endif
---
+++ FlameCsv.Core/Reading/Parallel/ICsvParallelInvoke.cs
@@ -0,0 +1,37 @@
+﻿#if FEATURE_PARALLEL
+using JetBrains.Annotations;
+
+namespace FlameCsv.Reading.Parallel;
+
+public interface ICsvAsyncInvoke<T> where T : unmanaged, IBinaryInteger<T>
+{
+    ValueTask InvokeAsync(CsvFieldsRef<T> fields, CsvParallelState state);
+}
+
+/// <summary>
+/// Interface for invoking an action on all CSV records in parallel.
+/// </summary>
+/// <typeparam name="T">Token type</typeparam>
+[PublicAPI]
+public interface ICsvParallelInvoke<T> where T : unmanaged, IBinaryInteger<T>
+{
+    /// <summary>
+    /// Invokes this instance on the specified fields.
+    /// </summary>
+    /// <param name="fields">Fields of the current CSV record</param>
+    /// <param name="state">State of the enumeration</param>
+    void Invoke(scoped ref CsvFieldsRef<T> fields, in CsvParallelState state);
+}
+
+internal readonly struct VoidParallelInvoke<T, TInvoke>(TInvoke invoke) : ICsvParallelTryInvoke<T, object?>
+    where T : unmanaged, IBinaryInteger<T>
+    where TInvoke : ICsvParallelInvoke<T>
+{
+    public bool TryInvoke(scoped ref CsvFieldsRef<T> fields, in CsvParallelState state, out object? result)
+    {
+        invoke.Invoke(ref fields, state);
+        result = null!;
+        return true;
+    }
+}
+#endif
