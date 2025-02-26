7084adc3fc2a5676d19edb4ad8d52d1d7c42f9cb
ovska <ovaskasi@gmail.com>
2025-02-26 21:14:57 +0200

    chore: remove unused code

--- FlameCsv.Core/Utilities/StringScratch.cs
+++ FlameCsv.Core/Utilities/StringScratch.cs
@@ -5,7 +5,7 @@ namespace FlameCsv.Utilities;
 [InlineArray(MaxLength)]
 internal struct StringScratch
 {
-    public string? elem0;
+    public string elem0;
     public const int MaxLength = 16;
 }
 
@@ -14,6 +14,6 @@ internal static class StringScratchExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Span<string> AsSpan(ref this StringScratch scratch, int length)
     {
-        return ((Span<string>)scratch!).Slice(0, length);
+        return ((Span<string>)scratch).Slice(0, length);
     }
 }
