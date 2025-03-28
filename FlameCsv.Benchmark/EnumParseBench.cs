5c12c81244b653341a007ab42558217fc86da620
ovska <ovaskasi@gmail.com>
2025-03-28 21:16:57 +0200

    bench: enumconverter source generator

--- FlameCsv.Benchmark/EnumParseBench.cs
+++ FlameCsv.Benchmark/EnumParseBench.cs
@@ -118,3 +118,39 @@ public partial class EnumParseBench
     [CsvEnumConverter<byte, TypeCode>]
     private partial class TypeCodeConverterByte;
 }
+
+/*
+| Method     | Bytes | IgnoreCase | ParseNumbers | Mean      | StdDev    | Ratio |
+|----------- |------ |----------- |------------- |----------:|----------:|------:|
+| TryParse   | False | False      | False        | 582.33 ns |  3.088 ns |  1.00 |
+| Reflection | False | False      | False        | 300.89 ns |  0.340 ns |  0.52 |
+| SourceGen  | False | False      | False        |  79.76 ns |  1.273 ns |  0.14 |
+|            |       |            |              |           |           |       |
+| TryParse   | False | False      | True         | 185.49 ns |  2.101 ns |  1.00 |
+| Reflection | False | False      | True         | 304.56 ns |  2.484 ns |  1.64 |
+| SourceGen  | False | False      | True         |  78.30 ns |  0.701 ns |  0.42 |
+|            |       |            |              |           |           |       |
+| TryParse   | False | True       | False        | 661.59 ns |  6.298 ns |  1.00 |
+| Reflection | False | True       | False        | 369.34 ns |  3.516 ns |  0.56 |
+| SourceGen  | False | True       | False        |  82.75 ns |  1.265 ns |  0.13 |
+|            |       |            |              |           |           |       |
+| TryParse   | False | True       | True         | 186.26 ns |  1.584 ns |  1.00 |
+| Reflection | False | True       | True         | 368.88 ns |  3.205 ns |  1.98 |
+| SourceGen  | False | True       | True         |  83.87 ns |  1.198 ns |  0.45 |
+|            |       |            |              |           |           |       |
+| TryParse   | True  | False      | False        | 726.99 ns | 15.936 ns |  1.00 |
+| Reflection | True  | False      | False        | 480.53 ns |  0.941 ns |  0.66 |
+| SourceGen  | True  | False      | False        |  73.65 ns |  0.433 ns |  0.10 |
+|            |       |            |              |           |           |       |
+| TryParse   | True  | False      | True         | 326.83 ns |  0.540 ns |  1.00 |
+| Reflection | True  | False      | True         | 485.12 ns |  4.999 ns |  1.48 |
+| SourceGen  | True  | False      | True         |  72.26 ns |  0.196 ns |  0.22 |
+|            |       |            |              |           |           |       |
+| TryParse   | True  | True       | False        | 785.22 ns |  1.791 ns |  1.00 |
+| Reflection | True  | True       | False        | 574.11 ns |  6.201 ns |  0.73 |
+| SourceGen  | True  | True       | False        |  72.89 ns |  0.869 ns |  0.09 |
+|            |       |            |              |           |           |       |
+| TryParse   | True  | True       | True         | 327.22 ns |  3.023 ns |  1.00 |
+| Reflection | True  | True       | True         | 560.96 ns |  5.796 ns |  1.71 |
+| SourceGen  | True  | True       | True         |  71.82 ns |  0.928 ns |  0.22 |
+ */
