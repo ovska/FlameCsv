c98ef19798cb9ce5048fc831fbf88000158b7261
ovska <ovaskasi@gmail.com>
2025-03-30 10:40:24 +0300

    test: move simdvector types to individual tests

--- FlameCsv.Tests/SimdVectorTests.cs
+++ FlameCsv.Tests/SimdVectorTests.cs
@@ -9,58 +9,31 @@ public static class SimdVectorTests
     private static ReadOnlySpan<byte> ByteData => "abcdefghijklmnopqrstuvwxyz0123456789"u8;
 
     [Fact]
-    public static void Should_Create_Vector()
-    {
-        Impl<byte, Vec128Byte>(ByteData, (v, b) => ((Vector128<byte>)v).CopyTo(b));
-        Impl<byte, Vec256Byte>(ByteData, (v, b) => ((Vector256<byte>)v).CopyTo(b));
-        // Impl<byte, Vec512Byte>(ByteData, (v, b) => ((Vector512<byte>)v).CopyTo(b));
-
-        Impl<char, Vec128Char>(CharData, (v, b) => ((Vector128<byte>)v).CopyTo(b));
-        Impl<char, Vec256Char>(CharData, (v, b) => ((Vector256<byte>)v).CopyTo(b));
-        // Impl<char, Vec512Char>(CharData, (v, b) => ((Vector512<byte>)v).CopyTo(b));
-    }
+    public static void Vector_128_Byte() => Impl<byte, Vec128Byte>(ByteData, (v, b) => ((Vector128<byte>)v).CopyTo(b));
 
     [Fact]
-    public static void Should_Handle_Non_Ascii_Chars()
-    {
-        foreach (var c in (char[]) [',', '"', '\n', '\r', '\0', '\t', '\\'])
-        {
-            Impl2<Vec128Char>(c);
-            Impl2<Vec256Char>(c);
-            // Impl2<Vec512Char>(c);
-        }
+    public static void Vector_256_Byte() => Impl<byte, Vec256Byte>(ByteData, (v, b) => ((Vector256<byte>)v).CopyTo(b));
 
-        static void Impl2<TVector>(char token) where TVector : struct, ISimdVector<char, TVector>
-        {
-            Assert.SkipUnless(TVector.IsSupported, $"CPU support not available for {typeof(TVector).Name}");
+    // [Fact]
+    // public static void Vector_512_Byte() => Impl<byte, Vec512Byte>(ByteData, (v, b) => ((Vector512<byte>)v).CopyTo(b));
 
-            var data = new string(
-                Enumerable
-                    .Range(0, (1024 * 8 / TVector.Count) + TVector.Count)
-                    .Select(i => i == token ? (char)(i + 1) : (char)i)
-                    .ToArray());
+    [Fact]
+    public static void Vector_128_Char() => Impl<char, Vec128Char>(CharData, (v, b) => ((Vector128<byte>)v).CopyTo(b));
 
-            for (int i = 0; i < data.Length; i += TVector.Count)
-            {
-                var vec = TVector.LoadUnaligned(in data.AsSpan()[i], 0);
-                var commaCheck = TVector.Create(token);
+    [Fact]
+    public static void Vector_256_Char() => Impl<char, Vec256Char>(CharData, (v, b) => ((Vector256<byte>)v).CopyTo(b));
 
-                var eq = TVector.Equals(vec, commaCheck);
+    // [Fact]
+    // public static void Vector_512_Char() => Impl<char, Vec512Char>(CharData, (v, b) => ((Vector512<byte>)v).CopyTo(b));
 
-                if (eq != TVector.Zero)
-                {
-                    char actual = data[i + (int)eq.ExtractMostSignificantBits()];
-                    Assert.Fail(
-                        $"Matched: {actual} ({(int)actual} / {(int)actual:X}) to {token} ({(int)token} / {(int)token:X})" +
-                        Environment.NewLine +
-                        $"Chars: [{data.AsSpan(i, TVector.Count)}], Bytes: " +
-                        string.Join(',', data.AsSpan(i, TVector.Count).ToArray().Select(x => ((int)x).ToString("X"))) +
-                        Environment.NewLine +
-                        $"Vec: {vec}");
-                }
-            }
-        }
-    }
+    [Fact]
+    public static void NonAscii_128() => Impl2<Vec128Char>();
+
+    [Fact]
+    public static void NonAscii_256() => Impl2<Vec256Char>();
+
+    // [Fact]
+    // public static void NonAscii_512() => Impl2<Vec512Char>();
 
     private static void Impl<T, TVector>(ReadOnlySpan<T> data, Action<TVector, Span<byte>> toBytes)
         where T : unmanaged, IBinaryInteger<T>
@@ -82,5 +55,44 @@ public static class SimdVectorTests
 
         toBytes(aVec, bytes);
         Assert.Equal(Enumerable.Repeat(byte.CreateChecked(data[0]), TVector.Count).ToArray(), bytes);
+    }
+
+    static void Impl2<TVector>() where TVector : struct, ISimdVector<char, TVector>
+    {
+        foreach (var c in (char[]) [',', '"', '\n', '\r', '\0', '\t', '\\'])
+        {
+            Impl2Core<TVector>(c);
+        }
+    }
+
+    static void Impl2Core<TVector>(char token) where TVector : struct, ISimdVector<char, TVector>
+    {
+        Assert.SkipUnless(TVector.IsSupported, $"CPU support not available for {typeof(TVector).Name}");
+
+        var data = new string(
+            Enumerable
+                .Range(0, (1024 * 8 / TVector.Count) + TVector.Count)
+                .Select(i => i == token ? (char)(i + 1) : (char)i)
+                .ToArray());
+
+        for (int i = 0; i < data.Length; i += TVector.Count)
+        {
+            var vec = TVector.LoadUnaligned(in data.AsSpan()[i], 0);
+            var commaCheck = TVector.Create(token);
+
+            var eq = TVector.Equals(vec, commaCheck);
+
+            if (eq != TVector.Zero)
+            {
+                char actual = data[i + (int)eq.ExtractMostSignificantBits()];
+                Assert.Fail(
+                    $"Matched: {actual} ({(int)actual} / {(int)actual:X}) to {token} ({(int)token} / {(int)token:X})" +
+                    Environment.NewLine +
+                    $"Chars: [{data.AsSpan(i, TVector.Count)}], Bytes: " +
+                    string.Join(',', data.AsSpan(i, TVector.Count).ToArray().Select(x => ((int)x).ToString("X"))) +
+                    Environment.NewLine +
+                    $"Vec: {vec}");
+            }
+        }
     }
 }
