afb5551ff7117652b65e6628dbc193aaeba44a4f
ovska <ovaskasi@gmail.com>
2025-04-10 16:09:46 +0300

    chore: delete custom char converters

--- FlameCsv.Core/Converters/DefaultConverters.cs
+++ FlameCsv.Core/Converters/DefaultConverters.cs
@@ -4,9 +4,6 @@ namespace FlameCsv.Converters;
 
 internal static class DefaultConverters
 {
-    private const NumberStyles Integer = NumberStyles.Integer;
-    private const NumberStyles Float = NumberStyles.Float;
-
     public static Func<CsvOptions<char>, CsvConverter<char>>? GetText(Type type)
     {
         if (type.IsEnum) return null;
@@ -14,26 +11,26 @@ internal static class DefaultConverters
             return o => o.StringPool is not null
                 ? new PoolingStringTextConverter(o)
                 : StringTextConverter.Instance;
-        if (type == typeof(Int32)) return o => new NumberTextConverter<int>(o, Integer);
-        if (type == typeof(Double)) return o => new NumberTextConverter<double>(o, Float);
+        if (type == typeof(Int32)) return o => new NumberTextConverter<int>(o, NumberStyles.Integer);
+        if (type == typeof(Double)) return o => new NumberTextConverter<double>(o, NumberStyles.Float);
         if (type == typeof(Boolean))
             return o => o.HasBooleanValues
                 ? new CustomBooleanTextConverter(o)
                 : BooleanTextConverter.Instance;
         if (type == typeof(DateTime)) return o => new SpanTextConverter<DateTime>(o);
-        if (type == typeof(Byte)) return o => new NumberTextConverter<byte>(o, Integer);
-        if (type == typeof(SByte)) return o => new NumberTextConverter<sbyte>(o, Integer);
-        if (type == typeof(Int16)) return o => new NumberTextConverter<short>(o, Integer);
-        if (type == typeof(UInt16)) return o => new NumberTextConverter<ushort>(o, Integer);
-        if (type == typeof(UInt32)) return o => new NumberTextConverter<uint>(o, Integer);
-        if (type == typeof(Int64)) return o => new NumberTextConverter<long>(o, Integer);
-        if (type == typeof(UInt64)) return o => new NumberTextConverter<ulong>(o, Integer);
-        if (type == typeof(Single)) return o => new NumberTextConverter<float>(o, Float);
-        if (type == typeof(Decimal)) return o => new NumberTextConverter<decimal>(o, Float);
+        if (type == typeof(Byte)) return o => new NumberTextConverter<byte>(o, NumberStyles.Integer);
+        if (type == typeof(SByte)) return o => new NumberTextConverter<sbyte>(o, NumberStyles.Integer);
+        if (type == typeof(Int16)) return o => new NumberTextConverter<short>(o, NumberStyles.Integer);
+        if (type == typeof(UInt16)) return o => new NumberTextConverter<ushort>(o, NumberStyles.Integer);
+        if (type == typeof(UInt32)) return o => new NumberTextConverter<uint>(o, NumberStyles.Integer);
+        if (type == typeof(Int64)) return o => new NumberTextConverter<long>(o, NumberStyles.Integer);
+        if (type == typeof(UInt64)) return o => new NumberTextConverter<ulong>(o, NumberStyles.Integer);
+        if (type == typeof(Single)) return o => new NumberTextConverter<float>(o, NumberStyles.Float);
+        if (type == typeof(Decimal)) return o => new NumberTextConverter<decimal>(o, NumberStyles.Float);
         if (type == typeof(DateTimeOffset)) return o => new SpanTextConverter<DateTimeOffset>(o);
         if (type == typeof(TimeSpan)) return o => new SpanTextConverter<TimeSpan>(o);
         if (type == typeof(Guid)) return o => new SpanTextConverter<Guid>(o);
-        if (type == typeof(Char)) return _ => CharTextConverter.Instance;
+        if (type == typeof(Char)) return o => new SpanTextConverter<char>(o);
         return null;
     }
 
@@ -44,26 +41,26 @@ internal static class DefaultConverters
             return o => o.StringPool is not null
                 ? new PoolingStringUtf8Converter(o)
                 : StringUtf8Converter.Instance;
-        if (type == typeof(Int32)) return o => new NumberUtf8Converter<int>(o, Integer);
-        if (type == typeof(Double)) return o => new NumberUtf8Converter<double>(o, Float);
+        if (type == typeof(Int32)) return o => new NumberUtf8Converter<int>(o, NumberStyles.Integer);
+        if (type == typeof(Double)) return o => new NumberUtf8Converter<double>(o, NumberStyles.Float);
         if (type == typeof(Boolean))
             return o => o.HasBooleanValues
                 ? new CustomBooleanUtf8Converter(o)
                 : BooleanUtf8Converter.Instance;
         if (type == typeof(DateTime)) return o => new SpanUtf8FormattableConverter<DateTime>(o);
-        if (type == typeof(Byte)) return o => new NumberUtf8Converter<byte>(o, Integer);
-        if (type == typeof(SByte)) return o => new NumberUtf8Converter<sbyte>(o, Integer);
-        if (type == typeof(Int16)) return o => new NumberUtf8Converter<short>(o, Integer);
-        if (type == typeof(UInt16)) return o => new NumberUtf8Converter<ushort>(o, Integer);
-        if (type == typeof(UInt32)) return o => new NumberUtf8Converter<uint>(o, Integer);
-        if (type == typeof(Int64)) return o => new NumberUtf8Converter<long>(o, Integer);
-        if (type == typeof(UInt64)) return o => new NumberUtf8Converter<ulong>(o, Integer);
-        if (type == typeof(Single)) return o => new NumberUtf8Converter<float>(o, Float);
-        if (type == typeof(Decimal)) return o => new NumberUtf8Converter<decimal>(o, Float);
+        if (type == typeof(Byte)) return o => new NumberUtf8Converter<byte>(o, NumberStyles.Integer);
+        if (type == typeof(SByte)) return o => new NumberUtf8Converter<sbyte>(o, NumberStyles.Integer);
+        if (type == typeof(Int16)) return o => new NumberUtf8Converter<short>(o, NumberStyles.Integer);
+        if (type == typeof(UInt16)) return o => new NumberUtf8Converter<ushort>(o, NumberStyles.Integer);
+        if (type == typeof(UInt32)) return o => new NumberUtf8Converter<uint>(o, NumberStyles.Integer);
+        if (type == typeof(Int64)) return o => new NumberUtf8Converter<long>(o, NumberStyles.Integer);
+        if (type == typeof(UInt64)) return o => new NumberUtf8Converter<ulong>(o, NumberStyles.Integer);
+        if (type == typeof(Single)) return o => new NumberUtf8Converter<float>(o, NumberStyles.Float);
+        if (type == typeof(Decimal)) return o => new NumberUtf8Converter<decimal>(o, NumberStyles.Float);
         if (type == typeof(DateTimeOffset)) return o => new SpanUtf8FormattableConverter<DateTimeOffset>(o);
         if (type == typeof(TimeSpan)) return o => new SpanUtf8FormattableConverter<TimeSpan>(o);
         if (type == typeof(Guid)) return o => new SpanUtf8FormattableConverter<Guid>(o);
-        if (type == typeof(Char)) return _ => CharUtf8Converter.Instance;
+        if (type == typeof(Char)) return o => new SpanUtf8Converter<char>(o);
         return null;
     }
 }
