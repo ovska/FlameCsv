def67a3b6c0b9a5ec403ee5a865ff45985043da0
ovska <ovaskasi@gmail.com>
2025-04-12 21:41:33 +0300

    fix: wrong attribute AllowMultiple configs

--- FlameCsv.Core/Attributes/CsvRequiredAttribute.cs
+++ FlameCsv.Core/Attributes/CsvRequiredAttribute.cs
@@ -4,7 +4,7 @@ namespace FlameCsv.Attributes;
 
 /// <summary>
 /// Marks the member or parameter as required when reading CSV.
-/// <c>init</c> properties and parameters without a default value are implicitly required.<br/>
+/// <c>required</c> or <c>init</c> properties, and parameters without a default value are implicitly required.<br/>
 /// When not placed on a member or parameter, <see cref="CsvFieldConfigurationAttribute.MemberName"/> must be set.<br/>
 /// When placed on an assembly, <see cref="CsvConfigurationAttribute.TargetType"/> must be set.
 /// </summary>
@@ -16,6 +16,6 @@ namespace FlameCsv.Attributes;
     AttributeTargets.Struct |
     AttributeTargets.Interface |
     AttributeTargets.Assembly,
-    AllowMultiple = true)]
+    AllowMultiple = true)] // type and assembly need AllMultiple = true
 [PublicAPI]
 public sealed class CsvRequiredAttribute : CsvFieldConfigurationAttribute;
