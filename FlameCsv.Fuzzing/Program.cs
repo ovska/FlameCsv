8a0616da91d4f4c69eb0e4babbe818e3d7d4d413
ovska <ovaskasi@gmail.com>
2025-04-15 20:53:02 +0300

    refactor: small perf improvements in CsvFields

--- FlameCsv.Fuzzing/Program.cs
+++ FlameCsv.Fuzzing/Program.cs
@@ -1,16 +1,16 @@
 ﻿#if true
-Fuzzer.LibFuzzer.Run(ScenarioRunner.Run<Converters>);
+Fuzzer.LibFuzzer.Run(ScenarioRunner.Run<Parsing>);
 #else
 DirectoryInfo dir = new(@"..\..\..\");
 
 // include files that start with crash- or timeout-
 var files = dir
-    .EnumerateFiles("crash-*", SearchOption.AllDirectories)
-    .Concat(dir.EnumerateFiles("timeout-*", SearchOption.AllDirectories));
+    .EnumerateFiles("crash-*", SearchOption.TopDirectoryOnly)
+    .Concat(dir.EnumerateFiles("timeout-*", SearchOption.TopDirectoryOnly));
 
 foreach (var file in files)
 {
     var data = File.ReadAllBytes(file.FullName);
-    ScenarioRunner.Run<Unescape>(data);
+    ScenarioRunner.Run<Parsing>(data);
 }
 #endif
