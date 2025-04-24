e5a29546f95526e6fb256f39c48de06f6d8496bc
ovska <ovaskasi@gmail.com>
2025-04-24 17:53:58 +0300

    chore: delete unused code

--- FlameCsv.Core/Metrics.cs
+++ FlameCsv.Core/Metrics.cs
@@ -1,38 +1,11 @@
-﻿using System.Buffers;
-using System.Diagnostics.Metrics;
-using System.Runtime.CompilerServices;
+﻿using System.Diagnostics.Metrics;
 
 namespace FlameCsv;
 
 internal static class Metrics
 {
     public const string MeterName = "FlameCsv";
+
+    // ReSharper disable once UnusedMember.Local
     private static readonly Meter _meter = new(MeterName);
-
-    private static readonly Counter<short> _tooLargeRent = _meter.CreateCounter<short>(
-        "memory.buffer_too_large",
-        description: "Required size for a temporary buffer was larger than the configured MemoryPool's maximum");
-
-    public static void TooLargeRent<T>(
-        int requiredSize,
-        MemoryPool<T> pool,
-        [CallerMemberName] string callsite = "")
-    {
-        // we end up boxing some value types, so check if the counter is enabled first
-        if (_tooLargeRent.Enabled)
-        {
-            _tooLargeRent.Add(
-                delta: 1,
-                new("required_size", requiredSize),
-                new("pool.type", pool.GetType().FullName),
-                new("pool.maximum", pool.MaxBufferSize),
-                new("callsite", callsite));
-        }
-    }
-
-    public static readonly Counter<long> parseAnyQuote = _meter.CreateCounter<long>(
-        "csv.parse_any_quote");
-
-    public static readonly Counter<long> parseAnyNewlineDelimiter = _meter.CreateCounter<long>(
-        "csv.parse_any_newline_delimiter");
 }
