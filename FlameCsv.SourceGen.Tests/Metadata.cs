c345bf4b0e02ae394b2e6adc536551a93e23603b
ovska <ovaskasi@gmail.com>
2025-04-30 20:30:11 +0300

    test(sourcegen): cache core assembly between tests

---
+++ FlameCsv.SourceGen.Tests/Metadata.cs
@@ -0,0 +1,38 @@
+using FlameCsv.Attributes;
+using Microsoft.CodeAnalysis;
+
+namespace FlameCsv.SourceGen.Tests;
+
+[CollectionDefinition(nameof(MetadataCollection))]
+public class MetadataCollection : ICollectionFixture<MetadataFixture>;
+
+public class MetadataFixture : IDisposable
+{
+    public MetadataReference FlameCsvCore
+    {
+        get
+        {
+            ObjectDisposedException.ThrowIf(_disposed, this);
+            return _assemblyMetadata.GetReference();
+        }
+    }
+
+    private readonly AssemblyMetadata _assemblyMetadata;
+    private bool _disposed;
+
+    public MetadataFixture()
+    {
+        // Initialize FlameCsvCore reference using AssemblyMetadata.CreateFromFile
+        string assemblyPath = typeof(CsvTypeMapAttribute<,>).Assembly.Location;
+        _assemblyMetadata = AssemblyMetadata.CreateFromFile(assemblyPath);
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        _disposed = true;
+        _assemblyMetadata.Dispose();
+    }
+}
