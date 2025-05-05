eb2ff222bd8d18209df59c07bd943681d65dcf72
ovska <ovaskasi@gmail.com>
2025-05-06 00:00:00 +0300

    feat: utility methods to write CsvRecords into CsvWriter

--- FlameCsv.Core/Exceptions/CsvWriteException.cs
+++ FlameCsv.Core/Exceptions/CsvWriteException.cs
@@ -6,6 +6,13 @@
 public sealed class CsvWriteException(string? message = null, Exception? innerException = null)
     : Exception(message, innerException)
 {
-    internal static CsvWriteException OnComplete(Exception ex) =>
-        new("Exception occured while writing leftover data while completing the writer.", ex);
+    /// <summary>
+    /// 1-based number of the record that caused the exception, if applicable.
+    /// </summary>
+    public int? LineNumber { get; init; }
+
+    internal static CsvWriteException OnComplete(Exception ex)
+    {
+        return new("Exception occured while writing leftover data while completing the writer.", ex);
+    }
 }
