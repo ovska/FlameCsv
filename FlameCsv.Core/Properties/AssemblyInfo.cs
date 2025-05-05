2e3d09c9ce35aaffa4ae9d771233ad6f173cee04
ovska <ovaskasi@gmail.com>
2025-05-06 00:00:00 +0300

    build: exclude InternalsVisibleToAttribute from release builds

--- FlameCsv.Core/Properties/AssemblyInfo.cs
+++ FlameCsv.Core/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-global using DAM = System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembersAttribute;
-global using RUF = System.Diagnostics.CodeAnalysis.RequiresUnreferencedCodeAttribute;
-global using RDC = System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute;
 global using System.Numerics;
+global using DAM = System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembersAttribute;
+global using RDC = System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute;
+global using RUF = System.Diagnostics.CodeAnalysis.RequiresUnreferencedCodeAttribute;
 
 [assembly: CLSCompliant(true)]
 
 [assembly: System.Reflection.Metadata.MetadataUpdateHandler(typeof(FlameCsv.Utilities.HotReloadService))]
 
-[assembly:
-    System.Diagnostics.CodeAnalysis.UnconditionalSuppressMessage(
-        "ReflectionAnalysis",
-        "IL3050",
-        Scope = "namespaceanddescendants",
-        Target = "~N:FastExpressionCompiler")]
+[assembly: System.Diagnostics.CodeAnalysis.UnconditionalSuppressMessage(
+    "ReflectionAnalysis",
+    "IL3050",
+    Scope = "namespaceanddescendants",
+    Target = "~N:FastExpressionCompiler"
+)]
