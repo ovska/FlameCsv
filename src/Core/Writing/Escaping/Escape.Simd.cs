9cc21f1e1eaca49e4a582e94556b4aa6869ee941
ovska <ovaskasi@gmail.com>
2025-05-12 22:17:20 +0300

    refactor: simplify unaligned TVector loading

--- src/Core/Writing/Escaping/Escape.Simd.cs
+++ src/Core/Writing/Escaping/Escape.Simd.cs
@@ -2,6 +2,7 @@ using System.Buffers;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using FlameCsv.Intrinsics;
 
 namespace FlameCsv.Writing.Escaping;
