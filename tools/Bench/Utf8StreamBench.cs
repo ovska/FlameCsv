74019a1855d7d6c40d310028e97739e09d8d1541
ovska <ovaskasi@gmail.com>
2026-01-19 15:58:30 +0200

    bench: custom utf8 stream writers and readers

--- tools/Bench/Utf8StreamBench.cs
+++ tools/Bench/Utf8StreamBench.cs
@@ -1,5 +1,7 @@
 ﻿using System.Buffers;
 using System.Text;
+using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Buffers;
 using FlameCsv.IO;
 using FlameCsv.IO.Internal;
 
@@ -7,13 +9,13 @@ using FlameCsv.IO.Internal;
 
 namespace FlameCsv.Benchmark;
 
-public class Utf8StreamBench
+public class Utf8StreamReadBench
 {
     [Params(false, true)]
     public bool Randomize { get; set; }
 
     [Benchmark(Baseline = true)]
-    public void _StreamReader()
+    public void _ReaderWrapper()
     {
         using var reader = GetReader();
 
@@ -46,10 +48,36 @@ public class Utf8StreamBench
         }
     }
 
+    [Benchmark]
+    public void _StreamReader()
+    {
+        using var reader = new StreamReader(
+            new MemoryStream(_data),
+            Encoding.UTF8,
+            bufferSize: CsvIOOptions.DefaultBufferSize
+        );
+
+        char[] buffer = ArrayPool<char>.Shared.Rent(CsvIOOptions.DefaultBufferSize);
+        int offsetIndex = 0;
+
+        while (true)
+        {
+            int endOffset = Randomize ? _offsets[offsetIndex++ % _offsets.Length] : 0;
+            int read = reader.Read(buffer.AsSpan(0, buffer.Length - endOffset));
+
+            if (read == 0)
+                break;
+        }
+
+        ArrayPool<char>.Shared.Return(buffer);
+    }
+
     private static readonly byte[] _data = File.ReadAllBytes("Comparisons/Data/65K_Records_Data.csv");
 
     private static ICsvBufferReader<char> GetReader() =>
-        CsvBufferReader.Create(new StreamReader(new MemoryStream(_data), Encoding.UTF8, bufferSize: 16 * 1024));
+        CsvBufferReader.Create(
+            new StreamReader(new MemoryStream(_data), Encoding.UTF8, bufferSize: CsvIOOptions.DefaultBufferSize)
+        );
 
     private static ICsvBufferReader<char> GetStream() => new Utf8StreamReader(new MemoryStream(_data), new());
 
@@ -57,4 +85,108 @@ public class Utf8StreamBench
         .Range(0, 1024)
         .Select(_ => Random.Shared.Next(0, 128))
         .ToArray();
+}
+
+public class Utf8StreamWriteBench
+{
+    // [Params(false, true)]
+    public bool Randomize { get; set; } = true;
+
+    [Benchmark(Baseline = true)]
+    public void _WriterWrapper()
+    {
+        var writer = GetWriter();
+        int offsetIndex = 0;
+        int written = 0;
+
+        while (written < 1024 * 1024 * 16)
+        {
+            int count = Randomize ? 64 + _offsets[offsetIndex++ % _offsets.Length] : 96;
+
+            Span<char> buffer = writer.GetSpan(count);
+            buffer.Clear();
+            writer.Advance(count);
+            written += count;
+
+            if (writer.NeedsFlush)
+            {
+                writer.Flush();
+            }
+        }
+
+        writer.Complete(null);
+    }
+
+    [Benchmark]
+    public void _Utf8StreamWriter()
+    {
+        var writer = GetStream();
+        int offsetIndex = 0;
+        int written = 0;
+
+        while (written < 1024 * 1024 * 16)
+        {
+            int count = Randomize ? 64 + _offsets[offsetIndex++ % _offsets.Length] : 96;
+
+            Span<char> buffer = writer.GetSpan(count);
+            buffer.Clear();
+            writer.Advance(count);
+            written += count;
+
+            if (writer.NeedsFlush)
+            {
+                writer.Flush();
+            }
+        }
+
+        writer.Complete(null);
+    }
+
+    [Benchmark]
+    public void _TextWriter()
+    {
+        using var writer = new StreamWriter(
+            GetDestination(),
+            Encoding.UTF8,
+            bufferSize: CsvIOOptions.DefaultBufferSize
+        );
+        int offsetIndex = 0;
+        int written = 0;
+
+        Span<char> buffer = stackalloc char[256];
+
+        while (written < 1024 * 1024 * 16)
+        {
+            int count = Randomize ? 64 + _offsets[offsetIndex++ % _offsets.Length] : 96;
+
+            var current = buffer.Slice(0, count);
+            current.Clear();
+            writer.Write(current);
+            written += count;
+            writer.Flush();
+        }
+    }
+
+    private static byte[] _data = (
+        (Func<byte[]>)(
+            () =>
+            {
+                byte[] data = new byte[1024 * 1024];
+                Random.Shared.NextBytes(data);
+                return data;
+            }
+        )
+    )();
+
+    private static ICsvBufferWriter<char> GetWriter() =>
+        new TextBufferWriter(
+            new StreamWriter(GetDestination(), Encoding.UTF8, bufferSize: CsvIOOptions.DefaultBufferSize),
+            new()
+        );
+
+    private static ICsvBufferWriter<char> GetStream() => new Utf8StreamWriter(GetDestination(), new());
+
+    private static readonly int[] _offsets = Enumerable.Range(0, 1024).Select(_ => Random.Shared.Next(0, 64)).ToArray();
+
+    private static Stream GetDestination() => new ArrayPoolBufferWriter<byte>().AsStream();
 }
