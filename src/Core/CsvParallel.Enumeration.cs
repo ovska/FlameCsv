9d7e2f57b7f20b9c6004bf5d550e39e55e890360
ovska <ovaskasi@gmail.com>
2026-01-21 15:16:51 +0200

    refactor(parallel): simplify enumerators a bit
    unwrap exception properly in sync parallel enumerator

--- src/Core/CsvParallel.Enumeration.cs
+++ src/Core/CsvParallel.Enumeration.cs
@@ -22,10 +22,13 @@ internal static partial class CsvParallel
         private readonly BlockingCollection<SlimList<T>> _blockingCollection;
         private SlimList<T>? _current;
 
+        /// <summary>Token to cancel the background tasks if enumeration ends early (e.g. via <c>break</c>)</summary>
         private readonly CancellationTokenSource _enumeratorCts;
-        private readonly CancellationTokenSource _pipelineCts; // user + enumerator
-        private readonly CancellationToken _pipelineToken;
 
+        /// <summary>Combined token for the entire pipeline (user + enumerator), early exit, user cancellation, or exception</summary>
+        private readonly CancellationTokenSource _pipelineCts;
+
+        /// <summary>Task running the parallel operation in the background</summary>
         private readonly Task _parallelTask;
         private bool _done;
 
@@ -41,30 +44,30 @@ internal static partial class CsvParallel
                 _enumeratorCts.Token
             );
 
-            _pipelineToken = _pipelineCts.Token;
-
             // the managers don't need to be disposed on exception as the pooling is only valid for the lifetime of the operation
             _blockingCollection = [];
 
-            EnumerationConsumer<T> consumer = new(_blockingCollection, null!, _pipelineToken);
+            EnumerationConsumer<T> consumer = new(_blockingCollection, null!, _pipelineCts.Token);
 
-            _parallelTask = Task.Factory.StartNew(
-                async () =>
-                {
-                    try
+            _parallelTask = Task
+                .Factory.StartNew(
+                    async () =>
                     {
-                        await parallelForEach(consumer, _pipelineToken).ConfigureAwait(false);
-                    }
-                    finally
-                    {
-                        Volatile.Write(ref _done, true);
-                        _blockingCollection.CompleteAdding();
-                    }
-                },
-                CancellationToken.None,
-                TaskCreationOptions.LongRunning,
-                parallelOptions.TaskScheduler ?? TaskScheduler.Default
-            );
+                        try
+                        {
+                            await parallelForEach(consumer, _pipelineCts.Token).ConfigureAwait(false);
+                        }
+                        finally
+                        {
+                            Interlocked.Exchange(ref _done, true);
+                            _blockingCollection.CompleteAdding();
+                        }
+                    },
+                    CancellationToken.None,
+                    TaskCreationOptions.LongRunning,
+                    parallelOptions.TaskScheduler ?? TaskScheduler.Default
+                )
+                .Unwrap();
         }
 
         public ArraySegment<T> Current => _current!.AsArraySegment();
@@ -72,16 +75,17 @@ internal static partial class CsvParallel
 
         public bool MoveNext()
         {
-            return _blockingCollection.TryTake(out _current, Timeout.Infinite, _pipelineToken);
+            return _blockingCollection.TryTake(out _current, Timeout.Infinite, _pipelineCts.Token);
         }
 
         public void Dispose()
         {
-            // If we haven't observed a terminal state yet, assume
-            // the consumer is quitting early -> signal our side.
-            if (!Volatile.Read(ref _done))
+            // if _done is not already true, we are exiting early via "break" in a foreach or similar
+            bool wasEarlyExit = !Interlocked.Exchange(ref _done, true);
+
+            // If we haven't observed a terminal state yet, assume the consumer is quitting early -> signal our side.
+            if (wasEarlyExit)
             {
-                // another thread might have canceled, but successive cancels are no-ops
                 _enumeratorCts.Cancel(throwOnFirstException: false);
             }
 
@@ -89,9 +93,9 @@ internal static partial class CsvParallel
             {
                 _parallelTask.GetAwaiter().GetResult();
             }
-            catch (OperationCanceledException) when (_enumeratorCts.IsCancellationRequested)
+            catch (OperationCanceledException) when (wasEarlyExit && _enumeratorCts.IsCancellationRequested)
             {
-                // Enumerator early-exit -> swallow
+                // swallow early exit cancellations
             }
             finally
             {
@@ -117,17 +121,24 @@ internal static partial class CsvParallel
 
     private sealed class ParallelAsyncEnumerator<T> : IAsyncEnumerator<ArraySegment<T>>
     {
+        /// <summary>
+        /// Channel that buffers the items not yet consumed by the enumerator.
+        /// </summary>
         private readonly Channel<SlimList<T>> _channel;
 
+        /// <summary>Token to cancel the background tasks if enumeration ends early (e.g. via <c>break</c>)</summary>
         private readonly CancellationTokenSource _enumeratorCts;
-        private readonly CancellationTokenSource _pipelineCts; // user + enumerator
-        private readonly CancellationToken _pipelineToken;
 
+        /// <summary>Combined token for the entire pipeline (user + enumerator), early exit, user cancellation, or exception</summary>
+        private readonly CancellationTokenSource _pipelineCts;
+
+        /// <summary>Task running the parallel operation in the background</summary>
         private readonly Task _parallelTask;
 
         public ArraySegment<T> Current => _current!.AsArraySegment();
 
         private SlimList<T>? _current;
+        private bool _done;
 
         public ParallelAsyncEnumerator(
             Func<IConsumer<SlimList<T>>, CancellationToken, Task> parallelForEachAsync,
@@ -137,7 +148,6 @@ internal static partial class CsvParallel
         {
             _enumeratorCts = new CancellationTokenSource();
             _pipelineCts = CancellationTokenSource.CreateLinked(userToken, getEnumeratorToken, _enumeratorCts.Token);
-            _pipelineToken = _pipelineCts.Token;
 
             _channel = Channel.CreateUnbounded<SlimList<T>>(
                 new UnboundedChannelOptions
@@ -148,14 +158,14 @@ internal static partial class CsvParallel
                 }
             );
 
-            EnumerationConsumer<T> consumer = new(null!, _channel.Writer, _pipelineToken);
+            EnumerationConsumer<T> consumer = new(null!, _channel.Writer, _pipelineCts.Token);
 
             _parallelTask = Task.Run(
                 async () =>
                 {
                     try
                     {
-                        await parallelForEachAsync(consumer, _pipelineToken).ConfigureAwait(false);
+                        await parallelForEachAsync(consumer, _pipelineCts.Token).ConfigureAwait(false);
                     }
                     finally
                     {
@@ -164,6 +174,8 @@ internal static partial class CsvParallel
                 },
                 CancellationToken.None
             );
+
+            _done = false;
         }
 
         public ValueTask<bool> MoveNextAsync()
@@ -178,7 +190,7 @@ internal static partial class CsvParallel
 
         private async ValueTask<bool> MoveNextWithAwait()
         {
-            while (await _channel.Reader.WaitToReadAsync(_pipelineToken).ConfigureAwait(false))
+            while (await _channel.Reader.WaitToReadAsync(_pipelineCts.Token).ConfigureAwait(false))
             {
                 if (_channel.Reader.TryRead(out _current))
                 {
@@ -187,20 +199,26 @@ internal static partial class CsvParallel
             }
 
             _current = null;
+            Volatile.Write(ref _done, true);
             return false;
         }
 
         public async ValueTask DisposeAsync()
         {
-            // If we haven't observed a terminal state yet, assume
-            // the consumer is quitting early -> signal our side.
-            _enumeratorCts.Cancel();
+            // if _done is not already true, we are exiting early via "break" in a foreach or similar
+            bool wasEarlyExit = !Interlocked.Exchange(ref _done, true);
+
+            // If we haven't observed a terminal state yet, assume the consumer is quitting early -> signal our side.
+            if (wasEarlyExit)
+            {
+                _enumeratorCts.Cancel(throwOnFirstException: false);
+            }
 
             try
             {
                 await _parallelTask.ConfigureAwait(false);
             }
-            catch (OperationCanceledException) when (_enumeratorCts.IsCancellationRequested)
+            catch (OperationCanceledException) when (wasEarlyExit && _enumeratorCts.IsCancellationRequested)
             {
                 // Enumerator early-exit -> swallow
             }
